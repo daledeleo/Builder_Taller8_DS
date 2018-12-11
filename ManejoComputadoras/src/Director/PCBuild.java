83afc442b259944983372f4fb64feec5f4b994fa
lindthon <lindthon4@hotmail.com>
2018-12-11 13:35:31 -0500

    Implementacion de PCBuild USUARIO MIGUEL ANGEL RIVAS

--- ManejoComputadoras/src/Director/PCBuild.java
+++ ManejoComputadoras/src/Director/PCBuild.java
@@ -2,4 +2,38 @@ package Director;
 
 public class PCBuild {
 
+	
+	private ComputadorBuilder contructorPC;
+	
+	
+	public void establecerContructor(ComputadorBuilder cp) {
+		
+		
+		this.contructorPC = cp;
+		
+	}
+	
+	
+	public void obtenerPC{
+		
+		
+		return contructorPC.obtenerPC();
+		
+		
+	}
+	
+	
+	public void construirPC() {
+		
+		
+		contructorPC.BuildInstalacionOs();
+		contructorPC.BuildInterna();
+		contructorPC.BuildExterna();
+		
+		
+	}
+	
+	
+	
+	
 }
